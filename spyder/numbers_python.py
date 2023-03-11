639808e3a5a95d89d235e1fd369380cc5577c4f8
Angelina Tan <angelinatan0701@gmail.com>
2023-03-12 01:01:29 +0700

    adding Python files

---
+++ spyder/numbers_python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 21:34:01 2023
+
+@author: angelina
+"""
+
+# Printing integers 1 to 10 without using loop 
+
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Printing integers 1 to 10 using loop 
+
+for i in range(1,11):
+    print(i)
+    
+for i in range(1,11):
+    print(i, end=" ")
+    
+x = 1 
+while x <= 10:
+    print(x)
+    x = x + 1 
